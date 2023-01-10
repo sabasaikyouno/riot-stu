45287f3b0311ffd9a1db5c4bfefe60c4e207c6a5
sabasaikyouno <0852kuro@gmail.com>
2023-01-10 19:34:36 +0900

    2

---
+++ src/main/scala/RiotCall.scala
@@ -0,0 +1,12 @@
+import Sleep._
+
+import scala.concurrent.duration._
+
+class RiotCall(apiKey: String) {
+  private val sleepCall = SleepCall((20, 1.seconds), (100, 2.minutes))
+
+  def getSummoner(name: String) = {
+    val url = s"https://jp1.api.riotgames.com/lol/summoner/v4/summoners/by-name/$name?api_key=$apiKey"
+    sleepCall.call(url)
+  }
+}

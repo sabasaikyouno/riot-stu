c64d8c78db4577e53f1b959885973f3e782b4ebf
sabasaikyouno <0852kuro@gmail.com>
2023-01-14 23:02:28 +0900

    2

--- src/main/scala/htt/Htt.scala
+++ src/main/scala/htt/Htt.scala
@@ -15,4 +15,19 @@ object Htt {
       .body()
       .string()
   }
+
+  def request(url: String, headers: (String, String)*) = {
+    val request = headers
+      .foldLeft(new Request.Builder())((request, header) =>
+        request.addHeader(header._1, header._2))
+      .url(url)
+      .build()
+
+    client
+      .newCall(request)
+      .execute()
+      .body()
+      .string()
+
+  }
 }
